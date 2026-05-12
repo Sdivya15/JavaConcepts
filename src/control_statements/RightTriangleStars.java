7a640a3e01291a4dee827a7bd36d02dfc0d0f31a
DivyaS <127476291+Sdivya15@users.noreply.github.com>
2026-05-12 06:36:24 +0530

    Added Examples of pattern printing using "for loops"

---
+++ src/control_statements/RightTriangleStars.java
@@ -0,0 +1,18 @@
+package control_statements;
+
+//*
+//**
+//***
+//****
+//*****
+
+public class RightTriangleStars {
+    public static void main(String[] args) {
+        for (int i =1; i <= 5; i++){
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
