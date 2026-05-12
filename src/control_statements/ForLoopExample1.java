dac95075818f9d45d6eb85441ad4d4fdfb1b8c5c
DivyaS <127476291+Sdivya15@users.noreply.github.com>
2026-05-12 06:09:09 +0530

    Added Examples Printing numbers using "for loops"

---
+++ src/control_statements/ForLoopExample1.java
@@ -0,0 +1,21 @@
+package control_statements;
+
+//1
+//12
+//123
+//1234
+//12345
+
+public class ForLoopExample1 {
+
+    public static void main(String args[]){
+        for(int i =1; i <= 5; i++){
+            //System.out.println(i + " ");
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j );
+            }
+            System.out.println();
+
+        }
+    }
+}
